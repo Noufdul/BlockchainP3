f1e37b286224b3eaf84990c4aff4bc8c18d055b5
Noufdul <49591354+Noufdul@users.noreply.github.com>
2019-06-17 21:14:44 +0300

    Add files via upload

---
+++ README[1].md
@@ -0,0 +1,8 @@
+# Express.JS Exercise with Node.js
+
+## Steps to testing 
+
+1. in Terminal node app.js 
+2. you will get msg " Server Listening for port: 8000 "
+3. check http://localhost:8000/block/0
+4. check http://localhost:8000/block/
---
+++ levelDBHelper[1].js
@@ -0,0 +1,21 @@
+module.exports = {
+  // Add data to levelDB with key/value pair
+  addLevelDBData: function(db, key, value){
+    return new Promise(function(resolve, reject) {
+      db.put(key, JSON.stringify(value), function(err) {
+        if (err) return console.log('Block ' + key + ' submission failed', err);
+        resolve(value)
+      });
+    });
+  },
+
+  // Get data from levelDB with key
+  getLevelDBData: function(db, key){
+    return new Promise(function(resolve, reject) {
+      db.get(key, function(err, value) {
+        if (err) return resolve(err);
+        resolve(value);
+      });
+    });
+  },
+}
